d3b9937eccb37835b546c94bb4d14d9d397cf217
slamdunk11 <ruret@naver.com>
2024-04-16 22:56:12 +0900

    feat: setup vitest with RTL

---
+++ src/__tests__/vitest_setup.ts
@@ -0,0 +1,12 @@
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import matchers from '@testing-library/jest-dom/matchers';
+
+// react-testing-library의 matcher를 확장한다.
+// `@testing-library/jest-dom`의 matcher를 사용할 수 있게 된다.
+expect.extend(matchers);
+
+// test 간 DOM 상태를 초기화
+afterEach(() => {
+  cleanup();
+});
